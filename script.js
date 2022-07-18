9a4c133df669d95081aca058b0f02f5e215aa02d
PrasathGowtham <108874931+PrasathGowtham@users.noreply.github.com>
2022-07-18 19:19:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let name1="Casino Royal";
+let name2=" Royal";
+let name3="Econ Production";
+let name4="GP Production";
+let name5="PG13";
+let name6="PG";
+
+
+
+let list = {tittle:{tittle1:name1,tittle1:name2},studio:{studio1:name3,studio1:name4},rating:{rating1:name5,rating2:name6}};
+
+
+class movie{
+constructor(list){
+this.tittle=list.tittle;
+this.studio=list.studio;
+this.rating=list.rating;
+}
+getPG(){
+return this.tittle + " " + this.studio + " " + this.rating;
+}
+
+
+}
+let movie1 = new movie(list);
+console.log(movie1.tittle.tittle1);
+console.log(movie1.studio.studio1);
+console.log(movie1.rating.rating1);
